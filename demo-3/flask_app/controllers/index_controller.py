81715f1ed286dc2fa28c0c4a2ed06986adcf2553
TasheemTheDream <114784701+TasheemTheDream@users.noreply.github.com>
2023-05-26 03:36:03 -0400

    Delete demo-3 directory

--- demo-3/flask_app/controllers/index_controller.py
+++
@@ -1,8 +0,0 @@
-from flask import redirect, render_template, request, send_from_directory, abort, jsonify, url_for
-from flask_app import app
-import os
-import uuid
-
-@app.route("/")
-def index():
-    return render_template("/pages/home.html")
--- demo-3/flask_app/static/login.js
+++
@@ -1,9 +0,0 @@
-var card = document.getElementById("card")
-
-function openRegister(){
-    card.style.transform = "rotateY(-180deg)";
-}
-
-function openLogin(){
-    card.style.transform = "rotateY(0deg)";
-}
